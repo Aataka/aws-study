80960d0b6051d169ed7b0b0c7f223be93485b284
aa_taka <otakahira@gmail.com>
2026-03-13 15:50:26 +0900

    feat: Terraform による構築成功を AWS CLI で実証済み

---
+++ provider.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  
+  # 重複していた3つの設定をここにまとめます
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  # S3のエラー（no such host）を解決するための重要な設定
+  s3_use_path_style           = true
+
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
+}
